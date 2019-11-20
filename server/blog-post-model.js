b8897495236ba775f6fc64c9114b266cfa2a3e18
javierescamilla <javier_escamillaj@hotmail.com>
2019-11-19 18:40:00 -0600

    Debugging 3

--- server/blog-post-model.js
+++ server/blog-post-model.js
@@ -1,53 +1,53 @@
-let mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
+// let mongoose = require('mongoose');
+// mongoose.Promise = global.Promise;
 
-let blogSchema = mongoose.Schema({
-    Breed : {type : String, required : true},
-    Color : {type : String},
-    Pictures : {type : String},
-    Found : {type : String},
-    Comments : {type : String}
-});
+// let blogSchema = mongoose.Schema({
+//     Breed : {type : String, required : true},
+//     Color : {type : String},
+//     Pictures : {type : String},
+//     Found : {type : String},
+//     Comments : {type : String}
+// });
 
-let Blog = mongoose.model('Blog', blogSchema);
+// let Blog = mongoose.model('Blog', blogSchema);
 
-let BlogList = {
-    get : function(){
-		return Blog.find()
-				.then( blogs => {
-					return blogs;
-				})
-				.catch( error => {
-					throw Error( error );
-				});
-	},
-    post : function(newBlog){
-        return Blog.create(newBlog)
-                .then( blog => {
-                    return blog;
-                })
-                .catch( err=> {
-                    throw Error(err);   
-                });
-    },
-    put : function(filer, updatedInfo){
-        return Blog.updateOne(filer, updatedInfo)
-                .then( blog => {
-                    return blog;
-                })
-                .catch( err=> {
-                    throw Error(err);   
-                });
-    },
-    delete :  function(filter){
-        return Blog.deleteOne(filter)
-            .then( blog => {
-                return blog;
-            })
-            .catch( err=> {
-                throw Error(err);   
-            });
-    }
-}
+// let BlogList = {
+//     get : function(){
+// 		return Blog.find()
+// 				.then( blogs => {
+// 					return blogs;
+// 				})
+// 				.catch( error => {
+// 					throw Error( error );
+// 				});
+// 	},
+//     post : function(newBlog){
+//         return Blog.create(newBlog)
+//                 .then( blog => {
+//                     return blog;
+//                 })
+//                 .catch( err=> {
+//                     throw Error(err);   
+//                 });
+//     },
+//     put : function(filer, updatedInfo){
+//         return Blog.updateOne(filer, updatedInfo)
+//                 .then( blog => {
+//                     return blog;
+//                 })
+//                 .catch( err=> {
+//                     throw Error(err);   
+//                 });
+//     },
+//     delete :  function(filter){
+//         return Blog.deleteOne(filter)
+//             .then( blog => {
+//                 return blog;
+//             })
+//             .catch( err=> {
+//                 throw Error(err);   
+//             });
+//     }
+// }
 
-module.exports = { BlogList };
+// module.exports = { BlogList };
--- server/server-blog-javier.js
+++ server/server-blog-javier.js
@@ -1,82 +1,82 @@
-let express = require ('express');
-let mongoose = require('mongoose');
-let morgan = require ('morgan');
+// let express = require ('express');
+// let mongoose = require('mongoose');
+// let morgan = require ('morgan');
 
-let app = express();
-let bodyParser = require( "body-parser" );
-let jsonParser = bodyParser.json();
+// let app = express();
+// let bodyParser = require( "body-parser" );
+// let jsonParser = bodyParser.json();
 
-let {BlogList} = require('./blog-post-model');
-let {DATABASE_URL, PORT} = require('./config');
-mongoose.Promise = global.Promise;
+// let {BlogList} = require('./blog-post-model');
+// let {DATABASE_URL, PORT} = require('./config');
+// mongoose.Promise = global.Promise;
 
-app.use(express.static('public'));
-app.use( morgan( 'dev' ) );
+// app.use(express.static('public'));
+// app.use( morgan( 'dev' ) );
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
-app.get( '/blog-posts', ( req, res, next ) => {
-    console.log("Enter get")
-    BlogList.get()
-    console.log(BlogList.get())
-		.then( blogs => {
-			return res.status( 200 ).json( blogs );
-		})
-		.catch( error => {
-			res.statusMessage = "Something went wrong with the DB. Try again later.";
-			return res.status( 500 ).json({
-				status : 500,
-				message : "Something went wrong with the DB. Try again later."
-			})
-		});
-});
+// app.get( '/blog-posts', ( req, res, next ) => {
+//     console.log("Enter get")
+//     BlogList.get()
+//     console.log(BlogList.get())
+// 		.then( blogs => {
+// 			return res.status( 200 ).json( blogs );
+// 		})
+// 		.catch( error => {
+// 			res.statusMessage = "Something went wrong with the DB. Try again later.";
+// 			return res.status( 500 ).json({
+// 				status : 500,
+// 				message : "Something went wrong with the DB. Try again later."
+// 			})
+// 		});
+// });
 
-let server;
+// let server;
 
-function runServer(port, databaseUrl){
-    return new Promise( (resolve, reject ) => {
-    mongoose.connect(databaseUrl, response => {
-    if ( response ){
-    return reject(response);
-    }
-    else{
-    server = app.listen(port, () => {
-    console.log( "App is running on port " + port );
-    resolve();
-    })
-    .on( 'error', err => {
-    mongoose.disconnect();
-    return reject(err);
-    })
-    }
-    });
-    });
-}
+// function runServer(port, databaseUrl){
+//     return new Promise( (resolve, reject ) => {
+//     mongoose.connect(databaseUrl, response => {
+//     if ( response ){
+//     return reject(response);
+//     }
+//     else{
+//     server = app.listen(port, () => {
+//     console.log( "App is running on port " + port );
+//     resolve();
+//     })
+//     .on( 'error', err => {
+//     mongoose.disconnect();
+//     return reject(err);
+//     })
+//     }
+//     });
+//     });
+// }
    
-function closeServer(){
-    return mongoose.disconnect()
-            .then(() => {
-return new Promise((resolve, reject) => {
-console.log('Closing the server');
-server.close( err => {
-if (err){
-    return reject(err);
-    }
-    else{
-    resolve();
-    }
-    });
-    });
-    });
-   }
+// function closeServer(){
+//     return mongoose.disconnect()
+//             .then(() => {
+// return new Promise((resolve, reject) => {
+// console.log('Closing the server');
+// server.close( err => {
+// if (err){
+//     return reject(err);
+//     }
+//     else{
+//     resolve();
+//     }
+//     });
+//     });
+//     });
+//    }
    
-runServer( PORT, DATABASE_URL )
-    .catch( err => {
-    console.log( err );
-});
+// runServer( PORT, DATABASE_URL )
+//     .catch( err => {
+//     console.log( err );
+// });
    
-module.exports = { app, runServer, closeServer };
+// module.exports = { app, runServer, closeServer };
